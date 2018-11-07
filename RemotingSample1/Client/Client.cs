2d4972b78a8903aefb37d69e9537cf5665138ec4
MaxJunior <maxwell989@gmail.com>
2018-11-07 07:06:38 +0000

    Change client cmd interface and implement a method to parse user cmd

--- RemotingSample1/Client/Client.cs
+++ RemotingSample1/Client/Client.cs
@@ -8,49 +8,57 @@ using System.Linq;
 
 namespace RemotingSample {
 
-	class Client {
+    class Client
+    {
 
         public static void exit()
         {
             Console.WriteLine("Exiting");
         }
 
-        static void Main() {
-            string command = "";
-            string s;
+        static void Main()
+        {
+
 
             TcpChannel channel = new TcpChannel();
-			ChannelServices.RegisterChannel(channel,true);
-            List<string> splited;
+            ChannelServices.RegisterChannel(channel, true);
+            //List<string> splited;
             List<List<Field>> lField;
 
 
-            MyRemoteObject obj = (MyRemoteObject) Activator.GetObject(
-				typeof(MyRemoteObject),
-				"tcp://localhost:8086/MyRemoteObjectName");
+            MyRemoteObject obj = (MyRemoteObject)Activator.GetObject(
+                typeof(MyRemoteObject),
+                "tcp://localhost:8086/MyRemoteObjectName");
 
-	 		try
-	 		{
-	 			Console.WriteLine(obj.MetodoOla());
+            try
+            {
+                //Console.WriteLine(obj.MetodoOla());
 
-                Console.WriteLine("Enter command <add / read / take> : ");
-                command = Console.ReadLine();
-                while (command != "exit")
+
+                while (true)
                 {
+                    Console.WriteLine("Write a command add/take/read <field1,field2,...,fieldn>  (exit:to leave) :");
+                    string user_input = Console.ReadLine();
+                    string[] cmd_params = user_input.Split(' ');
+                    // get the command
+                    string cmd = cmd_params[0];
+                    // given string : <field1,...,fieldN> get  just field1,...,fieldN
+                    string str_fields = cmd_params[1].Substring(1, cmd_params[1].Length - 1);
+                    // wilcardValue
+                    int wildValue = 0;
+                    // get the array of fields
+                    List<string> field_list = argumentParser(str_fields, ref wildValue);
+                    bool endProcessCommand = false;
 
-                    switch (command)
+                    switch (cmd)
                     {
                         case "add":
-                            Console.WriteLine("Insert tuple schema, ex: ola;DADTestA(1,'a')");
-                            s = Console.ReadLine();
-                            splited = s.Split(';').ToList();
-                            obj.add(splited);
+
+                            obj.add(field_list);
                             break;
                         case "read":
-                            Console.WriteLine("Insert tuple schema, ex: ola;DADTestA(1,a)");
-                            s = Console.ReadLine();
-                            splited = s.Split(';').ToList();
-                            lField = obj.readTuple(splited);
+
+                            lField = obj.readTuple(field_list);
                             foreach (List<Field> ls in lField)
                                 foreach (Field f in ls)
                                 {
@@ -64,20 +72,74 @@ namespace RemotingSample {
                         case "take":
                             Console.WriteLine("Case 2");
                             break;
+                        case "exit":
+                            endProcessCommand = true; break;
+                        default: Console.WriteLine("Invalid Command : {0}. Please,use the correct command syntax.", cmd); break;
                     }
-                    Console.WriteLine("Enter command:");
-                    command = Console.ReadLine();
+                    if (endProcessCommand)
+                        exit();
                 }
-                exit();
 
-             }
-	 		catch(SocketException)
-	 		{
-	 			System.Console.WriteLine("Could not locate server");
-	 		}
 
-		}
+            }
+            catch (SocketException)
+            {
+                System.Console.WriteLine("Could not locate server");
+            }
 
-        
+        }
+        static List<string> argumentParser(string fields, ref int wilcardFounded)
+        {
+            List<string> argsList = new List<string>();
+            int bracket_counter = 0;
+
+            string cur_field = "";
+            for (int i = 0; i < fields.Length; i++)
+            {
+                //Console.WriteLine("MEW : {0} kkkkkkk  Value {1}", i, fields[i]);
+                if (fields[i] == ',' && bracket_counter == 0)
+                {
+                    //Console.WriteLine(cur_field);
+                    argsList.Add(cur_field);
+
+
+                    cur_field = "";
+                }
+                else if (fields[i] == '(')
+                {
+                    cur_field += fields[i];
+                    bracket_counter++;
+                }
+                else if (fields[i] == ',' && bracket_counter > 0)
+                {
+                    cur_field += fields[i];
+
+                }
+                else if (fields[i] == ')')
+                {
+                    cur_field += fields[i];
+                    argsList.Add(cur_field);
+                    //Console.WriteLine("Addd : {0}", cur_field);
+                    cur_field = "";
+                    bracket_counter = 0;
+                }
+                else
+                {
+                    cur_field += fields[i];
+                    if (i == fields.Length - 1)
+                        argsList.Add(cur_field);
+
+                    if (fields[i] == '*')
+                    {
+                        Console.WriteLine("WilDCARD FOUND");
+                        wilcardFounded = (cur_field == "\""  || cur_field == @"'") ? 1 : 2;
+                        //Console.WriteLine("W : {0}", wilcardFounded);
+                    }
+                    //Console.WriteLine("Else  {0} ...",cur_field);
+                }
+            }
+            return argsList;
+        }
+
     }
 }
